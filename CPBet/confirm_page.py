c686b3b646a3379fc5e7d0a3bf67c610416f7846
lsorin81 <lsorin81@gmail.com>
2013-10-15 18:12:07 +0300

    marathon part 1

--- CPBet/confirm_page.py
+++ CPBet/confirm_page.py
@@ -1,4 +1,4 @@
-from CPBet.models import unique_token_key
+from CPBet.models import unique_token_key, bookmakers
 
 __author__ = 'tippytip'
 #StdLib imports
@@ -25,6 +25,7 @@ ARG_BET_DATE = "BetDate"
 ARG_BET_TYPE = "BetType"
 ARG_BET_RESULT = "BetResult"
 ARG_DICTIONARY = "Dictionary"
+ARG_BOOKMAKER_PICTURE = "BookmakerPicture"
 POST_BET = "Bet"
 POST_TOKEN = "Token"
 ID_BET365 = 0
@@ -36,34 +37,21 @@ REGEX_PINNACLE_ODDS = re.compile(r"\d+\.\d\d\d\s[ADF]")
 SECRET = "CopyPasteBet"
 STATUS_TOKEN_PARSING_ERROR = 460
 ERR_PARSE_TOKEN = "Error parsing the token"
+STANDARD_MARATHON_NO = 16;
 
 # when we copy the date from other websites the dates come from other world regions
 # pending and settled
-DUMMY_BET = {
-    ARG_EVENT_DESCRIPTION: "Fulham -vs- Stoke City",
-    ARG_EVENT_DATE: "10/5/2013 7:00",
-    ARG_EVENT_RESULT: "FT 1:0",
-    ARG_BET_DESCRIPTION: "Fulham",
-    ARG_BET_CODE: "327237505",
-    ARG_BET_BOOKMAKER: "PinnacleSports",
-    ARG_BET_ODDS: 2.640,
-    ARG_BET_AMOUNT: 10.00,
-    ARG_BET_DATE: "10/5/2013 6:48",
-    ARG_BET_TYPE: "Game - 1X2",
-    ARG_BET_RESULT: 5
-}
 
 
 def confirm(request):
     unprocessedString = request.POST.get(POST_BET)
     token = request.POST.get(POST_TOKEN)
-    if type(token) == unicode:
-    # encode() without parameters transforms it to String
-        token = token.encode()
     try:
+        if type(token) == unicode:
+        # encode() without parameters transforms it to String
+            token = token.encode()
         secret = unique_token_key.objects.get(pk=1).key
         data = tokenlib.parse_token(token, secret=secret)
-        # here we make up the restaurant data
         key = unicode("userid")
     except:
         return HttpResponse(json.dumps({"Status": STATUS_TOKEN_PARSING_ERROR,
@@ -71,9 +59,26 @@ def confirm(request):
     tabArray = printTabbedArrays(unprocessedString)
     if len(tabArray) == 1:
     # MarathonBet doesn't contain tabs
-        picture = ID_MARATHON_BET
-    # printDateArrays(dummies)
-    return render_to_response('CPBet/confirm.html', DUMMY_BET, context_instance=RequestContext(request))
+        marathonArray = tabArray[0].split(" ")
+        if len(marathonArray) == STANDARD_MARATHON_NO:
+            betFields = {
+                ARG_BOOKMAKER_PICTURE: stripUrl(bookmakers.objects.get(pk=4).image.url),
+                ARG_EVENT_DESCRIPTION: "Fulham -vs- Stoke City",
+                ARG_EVENT_DATE: "10/5/2013 7:00",
+                ARG_EVENT_RESULT: "FT 1:0",
+                ARG_BET_DESCRIPTION: "Fulham",
+                ARG_BET_CODE: "327237505",
+                ARG_BET_ODDS: 2.640,
+                ARG_BET_AMOUNT: 10.00,
+                ARG_BET_DATE: "10/5/2013 6:48",
+                ARG_BET_TYPE: "Game - 1X2",
+                ARG_BET_RESULT: 5
+            }
+            return render_to_response('CPBet/confirm.html', betFields, context_instance=RequestContext(request))
+        else:
+            return HttpResponse(json.dumps({"Status": 400,
+                                            "Error": "Not a proper MarathonBet"},
+                                           sort_keys=True))
 
 
 def identifyPinnacle(unprocessedString):
@@ -102,6 +107,11 @@ def hasDate(string):
     if re.match(r".+\d:\d\d.+", string):
         return True
     return False
+
+
+def stripUrl(urlString):
+    urlArray = urlString.split("/")
+    return urlArray[len(urlArray)-1]
 
 
 # check if it has Pinnacle odds style
